bfec97702043352b61384cb4bc63991688977cb7
นันทโชต โมราศรี <624259017@webmail.npru.ac.th>
2021-07-19 16:03:22 +0700

    Initial commit

---
+++ src/Honda.java
@@ -0,0 +1,26 @@
+class Honda extends Car{
+
+    public void HondaStart(){
+
+        CarEngine Honda_Engine = new CarEngine(); //composition
+
+        Honda_Engine.startEngine();
+
+    }
+
+	public void setColour(String colour) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void carDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setMaxSpeed(int maxi_Speed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+} 
